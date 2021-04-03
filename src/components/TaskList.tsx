6fa4ea127839e8ee742518d3a7f85f06f239fb29
Wagner Rodrigues <wagnersr92@gmail.com>
2021-04-03 19:32:23 -0300

    Challenge complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,35 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  function handleCreateNewTask() {    
+    if (!newTaskTitle){
+      return new Error('Informe um titulo para a task');
+    } 
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState =>  [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTasks = tasks.map(task => task.id === id ?{
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(updateTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks); 
   }
 
   return (
